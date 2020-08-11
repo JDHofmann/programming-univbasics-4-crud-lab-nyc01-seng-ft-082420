c38a9f36de546182fe20fa7d29ef19714252434f
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-11 18:01:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  second_array = ["Tom", "Zarro", "JD", "Amissa"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  second_array.push("Zora")
 end
 
 def add_element_to_start_of_array(array, element)
